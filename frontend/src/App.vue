9895d888e1a9cbc0812c137255a1587a2da09518
Katherine De Leon <deleonkath47@gmail.com>
2025-01-20 20:07:19 +0800

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-blue-500 text-white p-4 m-4 rounded-lg">
+    <h1 class="text-2xl font-bold">Hello, Tailwind!</h1>
+    <p class="mt-2">If you can see this styled, Tailwind is working!</p>
+  </div>
+</template>
